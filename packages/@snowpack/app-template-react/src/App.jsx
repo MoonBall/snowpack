c2c97028e6b1d6627c545101a284f445aa5bdc33
Chen Gang <gangc.cxy@foxmail.com>
2020-08-14 11:10:23 +0800

    example for `support-alias-node_modules-assets`

--- packages/@snowpack/app-template-react/src/App.jsx
+++ packages/@snowpack/app-template-react/src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import adSvg from 'fasvgs/solid/ad.svg';
 
 function App() {
   return (
     <div className="App">
+      <img src={adSvg} style={{ width: 24, height: 24 }} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
